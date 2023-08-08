a1ecfd754f951973c74a8fa75d63b1a3ab6007af
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2023-08-08 20:45:46 -0300

    Create Product and Category models and migrations

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    quantity = models.IntegerField(default=0)
